2aadd5da683efb800c78dd9f80d6e331debfa83d
An Nguyen <annguyen@kr.af.mil>
2020-12-14 23:12:34 -0500

    Played around with React-Flow visualization tool

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
+import { AppBar, IconButton, Toolbar, Typography } from '@material-ui/core';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import React, { useState } from 'react';
+import ReactFlow, { Background, Controls } from 'react-flow-renderer';
 import './App.css';
+import MenuIcon from '@material-ui/icons/Menu';
 
-function App() {
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }),
+);
+
+export const App = () => {
+  const elements = [
+    {
+      id: '1',
+      type: 'input', // input node
+      data: { label: 'Input Node' },
+      position: { x: 250, y: 25 },
+    },
+    // default node
+    {
+      id: '2',
+      // you can also pass a React component as a label
+      data: { label: <div>Default Node</div> },
+      position: { x: 100, y: 125 },
+    },
+    {
+      id: '3',
+      type: 'output', // output node
+      data: { label: 'Output Node' },
+      position: { x: 250, y: 250 },
+    },
+    // animated edge
+    { id: 'e1-2', source: '1', target: '2', animated: true },
+    { id: 'e2-3', source: '2', target: '3' },
+  ];
+
+  const classes = useStyles();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar position='static'>
+        <Toolbar>
+        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" className={classes.title}>
+          Datastructure Visualizer
+        </Typography>
+        </Toolbar>
+      </AppBar>
+
+
+      <Typography>YO WHERE MY NODES AT???</Typography>
+      <ReactFlow
+        elements={elements}
+      >
+        <Controls />
+        <Background color="#aaa" gap={16} />
+      </ReactFlow>
     </div>
   );
 }
